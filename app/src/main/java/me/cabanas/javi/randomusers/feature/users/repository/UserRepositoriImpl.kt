fbfc600d57358344a422fd057543f1cff932d068
Juan Javier Cabanas <juan.javier.cabanas@gmail.com>
2018-09-30 19:00:29 +0200

    implement user repository and define network datasource

---
+++ app/src/main/java/me/cabanas/javi/randomusers/feature/users/repository/UserRepositoriImpl.kt
@@ -0,0 +1,11 @@
+package me.cabanas.javi.randomusers.feature.users.repository
+
+import me.cabanas.javi.randomusers.core.functional.Either
+import me.cabanas.javi.randomusers.feature.users.domain.interactors.ReadUserListInteractor
+import me.cabanas.javi.randomusers.feature.users.domain.model.UserEntity
+import me.cabanas.javi.randomusers.feature.users.repository.network.UserNetworkDatasource
+
+class UserRepositoriImpl(val network: UserNetworkDatasource) : UserRepository {
+    override fun readContactList(request: ReadUserListInteractor.UserListRequest):
+            Either<Error, List<UserEntity>> = network.readContactList(request)
+}
