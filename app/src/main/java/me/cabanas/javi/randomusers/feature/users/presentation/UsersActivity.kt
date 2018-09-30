cd421e4e8dcfd101ac5013a89476f45fcb22baf4
Juan Javier Cabanas <juan.javier.cabanas@gmail.com>
2018-09-30 18:20:35 +0200

    start project Create base and users activities and its layouts

---
+++ app/src/main/java/me/cabanas/javi/randomusers/feature/users/presentation/UsersActivity.kt
@@ -0,0 +1,8 @@
+package me.cabanas.javi.randomusers.feature.users.presentation
+
+import me.cabanas.javi.randomusers.R
+import me.cabanas.javi.randomusers.ui.base.BaseActivity
+
+class UsersActivity:BaseActivity() {
+    override fun getLayoutResourceId(): Int = R.layout.activity_users
+}
---
+++ app/src/main/java/me/cabanas/javi/randomusers/ui/base/BaseActivity.kt
@@ -0,0 +1,34 @@
+package me.cabanas.javi.randomusers.ui.base
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import kotlinx.android.synthetic.main.activity_base.*
+import me.cabanas.javi.randomusers.R
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        inflateLayout()
+    }
+
+    private fun inflateLayout() {
+        if (getBaseLayout() == 0) {
+            setContentView(getLayoutResourceId())
+        } else {
+            setContentView(getBaseLayout())
+            val view = LayoutInflater.from(this)
+                    .inflate(getLayoutResourceId(), content, false)
+            content.addView(view)
+        }
+    }
+
+    abstract fun getLayoutResourceId(): Int
+
+    protected fun getBaseLayout() = R.layout.activity_base
+
+    open protected fun initView() {
+
+    }
+}
