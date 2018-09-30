ac4d3c67ce87219308e42ee2bde97a99383308b5
Juan Javier Cabanas <juan.javier.cabanas@gmail.com>
2018-09-30 18:38:03 +0200

    create user models

---
+++ app/src/main/java/me/cabanas/javi/randomusers/feature/users/domain/model/UserModels.kt
@@ -0,0 +1,27 @@
+package me.cabanas.javi.randomusers.feature.users.domain.model
+
+import com.google.gson.annotations.SerializedName
+
+data class UserEntity(@SerializedName("gender") val gender: String,
+                      @SerializedName("name") val name: NameEntity,
+                      @SerializedName("location") val location: LocationEntity,
+                      @SerializedName("email") val email: String,
+                      @SerializedName("phone") val phone: String,
+                      @SerializedName("cell") val cell: String,
+                      @SerializedName("picture") val picture: PictureEntity,
+                      @SerializedName("registered") val registered: RegisteredEntity)
+
+data class NameEntity(@SerializedName("first") val first: String,
+                      @SerializedName("last") val last: String)
+
+data class LocationEntity(@SerializedName("street") val street: String,
+                          @SerializedName("city") val city: String,
+                          @SerializedName("state") val state: String,
+                          @SerializedName("postcode") val postcode: String)
+
+data class PictureEntity(@SerializedName("large") val large: String,
+                         @SerializedName("medium") val medium: String,
+                         @SerializedName("thumbnail") val thumbnail: String)
+
+data class RegisteredEntity(@SerializedName("date") val date: String,
+                            @SerializedName("age") val age: Int)
