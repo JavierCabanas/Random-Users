29e60629f7a163a14463e743e177ea6dc78e4285
Juan Javier Cabanas <juan.javier.cabanas@gmail.com>
2018-09-30 20:25:36 +0200

    update viewmodel to read users from interactor

--- app/src/main/java/me/cabanas/javi/randomusers/feature/users/presentation/UsersViewModel.kt
+++ app/src/main/java/me/cabanas/javi/randomusers/feature/users/presentation/UsersViewModel.kt
@@ -2,21 +2,28 @@ package me.cabanas.javi.randomusers.feature.users.presentation
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import me.cabanas.javi.randomusers.core.error.Failure
+import me.cabanas.javi.randomusers.feature.users.domain.interactors.ReadUserListInteractor
 import me.cabanas.javi.randomusers.feature.users.domain.model.*
 
-class UsersViewModel : ViewModel() {
+class UsersViewModel(private val readUsers: ReadUserListInteractor) : ViewModel() {
 
     var userList = mutableListOf<UserEntity>()
     var users: MutableLiveData<List<UserEntity>> = MutableLiveData()
 
     private var page = 0
 
-    fun loadUsers() {
-        userList.addAll(listOf<UserEntity>(UserEntity("male", NameEntity("javi", "canamas"),
-                LocationEntity("", "", "", ""), "jhasjkd@askldj.com",
-                "12347", "123456", PictureEntity("", "", ""),
-                RegisteredEntity("", 0))))
+    fun loadUsers() = readUsers(ReadUserListInteractor.UserListRequest(page, 10)) {
+        it.either(::handleFailure, ::handleSuccess)
+    }
+
+    private fun handleSuccess(result: List<UserEntity>) {
+        userList.addAll(result)
         users.value = userList.distinctBy { it.email }
+    }
+
+    private fun handleFailure(failure: Failure) {
+
     }
 
 }
